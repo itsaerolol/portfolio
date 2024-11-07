437ea7dd441526e16cf1ac486d8e340b41c1d196
Keaton White <keatnkeat3752@gmail.com>
2024-11-07 09:24:56 -0500

    Text Editing; added and updated fonts with Geist (Vercel Font)

--- app/components/IntroSection.jsx
+++ app/components/IntroSection.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation';
 
+
 const IntroSection = () => {
   return (
     <section>
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-cente sm:text-left">
-                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                   <span className="text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-orange-800">
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:mt-16 md:mt-8 font-extrabold">
+                   <span className="text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-orange-800 font-sans">
                       A Look into {" "}
                     </span>
                     <div>
@@ -34,19 +35,21 @@ const IntroSection = () => {
 
                 </h1>
                 <p className="text-[#ADB7BE] text-base mb-6 sm:text-lg lg:text-xl">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veer into my perspective of this digital artform.
                 </p>
                 <div>
                 <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-orange-500 to-orange-800 hover:bg-slate-200 text-white'>
-                  Hire Me
+                  Recent Projects
                 </button>
-                <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-orange-500 to-orange-800 hover:bg-slate-800 text-white mt-3'>
-                  <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
+                <button id="contact-button" className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-orange-500 to-orange-800 hover:bg-slate-800 text-white mt-3'>
+                  <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+                      Career Path
+                  </span>
                 </button>
                 </div>
             </div>
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                <div className="rounded-full bg-[#282828] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]">
+                <div className="sm:ml-28 lg:mr-12 rounded-full bg-[#282828] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]">
                   <Image
                     src="/img/camera.png"
                     alt="self image"
